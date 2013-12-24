e9105a0b042d65cd0ba10b1894b0663737171c33
Marco Nguitragool-Schulze <marco@codewizards.co>
2013-12-24 10:18:44 +0100

    Ported a few useful things from NightLabs' codebase.

--- co.codewizards.cloudstore.shared/src/main/java/co/codewizards/cloudstore/shared/util/ObjectUtil.java
+++ co.codewizards.cloudstore.shared/src/main/java/co/codewizards/cloudstore/shared/util/ObjectUtil.java
@@ -1,14 +1,7 @@
 package co.codewizards.cloudstore.shared.util;
 
-public class ObjectUtil {
-	public static <T> T assertNotNull(String name, T object) {
-		if (object == null)
-			throw new IllegalArgumentException(String.format("%s == null", name));
-
-		return object;
-	}
-
-	public static boolean equals(Object one, Object two) {
-		return one == null ? two == null : one.equals(two);
-	}
+/**
+ * @deprecated Use Util instead!
+ */
+public class ObjectUtil extends Util {
 }
