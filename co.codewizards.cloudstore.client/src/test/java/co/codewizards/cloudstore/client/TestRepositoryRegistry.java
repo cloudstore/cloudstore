a380c1eb22debed76af1a255e8a020feac6338b7
chairatk <chairat@guinaree.com>
2013-12-25 16:23:44 +0700

    Modified wrong names in the pom and added a test method.

---
+++ co.codewizards.cloudstore.client/src/test/java/co/codewizards/cloudstore/client/TestRepositoryRegistry.java
@@ -0,0 +1,21 @@
+package co.codewizards.cloudstore.client;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import co.codewizards.cloudstore.shared.repo.RepositoryManagerRegistry;
+
+public class TestRepositoryRegistry {
+	private static final Logger logger = LoggerFactory.getLogger(TestRepositoryRegistry.class);
+	@Test
+	public void createRepository() {
+		RepositoryManagerRegistry repositoryManagerRegistry = RepositoryManagerRegistry.getInstance();
+
+		File target = new File("target");
+		logger.trace("LocalRoot: " + target.getAbsolutePath());
+		repositoryManagerRegistry.getRepositoryManager(target);
+	}
+}
