376bc68cf292a6b0d9081b4c42a45e7868453f19
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-01-05 18:14:11 +0100

    Cleaned up code.

--- co.codewizards.cloudstore.server/src/main/java/co/codewizards/cloudstore/server/CloudStoreServer.java
+++ co.codewizards.cloudstore.server/src/main/java/co/codewizards/cloudstore/server/CloudStoreServer.java
@@ -20,6 +20,7 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.bouncycastle.jce.X509Principal;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -35,11 +36,19 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import co.codewizards.cloudstore.core.config.ConfigDir;
 import co.codewizards.cloudstore.rest.server.CloudStoreREST;
 
 public class CloudStoreServer implements Runnable {
+	public static final String SYSTEM_PROPERTY_SECURE_PORT = "cloudstore.securePort";
+
+	private static final Logger logger = LoggerFactory.getLogger(CloudStoreServer.class);
+
+	private static final int DEFAULT_SECURE_PORT = 8443;
+
 	private static final String CERTIFICATE_ALIAS = "CloudStoreServer";
 	private static final String CERTIFICATE_COMMON_NAME = CERTIFICATE_ALIAS;
 
@@ -51,16 +60,18 @@ public class CloudStoreServer implements Runnable {
 
 	private File keyStoreFile;
 	private SecureRandom random = new SecureRandom();
+	private int securePort;
+	private final AtomicBoolean running = new AtomicBoolean();
 
 	public static void main(String[] args) {
 		new CloudStoreServer().run();
 	}
-	
+
 	public CloudStoreServer() {
 		Provider provider = Security.getProvider("BC");
 		if (provider == null)
 			Security.addProvider(new BouncyCastleProvider());
-		
+
 		provider = Security.getProvider("BC");
 		if (provider == null)
 			throw new IllegalStateException("Registration of BouncyCastleProvider failed!");
@@ -68,6 +79,9 @@ public class CloudStoreServer implements Runnable {
 
 	@Override
 	public void run() {
+		if (!running.compareAndSet(false, true))
+			throw new IllegalStateException("Server is already running!");
+
 		try {
 			initKeyStore();
 			Server server = createServer();
@@ -77,6 +91,8 @@ public class CloudStoreServer implements Runnable {
 			throw x;
 		} catch (Exception x) {
 			throw new RuntimeException(x);
+		} finally {
+			running.set(false);
 		}
 	}
 
@@ -95,6 +111,46 @@ public class CloudStoreServer implements Runnable {
 		return keyStoreFile;
 	}
 
+	public synchronized void setKeyStoreFile(File keyStoreFile) {
+		assertNotRunning();
+		this.keyStoreFile = keyStoreFile;
+	}
+
+	public synchronized int getSecurePort() {
+		if (securePort <= 0) {
+			String value = System.getProperty(SYSTEM_PROPERTY_SECURE_PORT);
+			if (value == null) {
+				logger.debug("System property '{}' not set. Listening on default port {}.", SYSTEM_PROPERTY_SECURE_PORT, DEFAULT_SECURE_PORT);
+				securePort = DEFAULT_SECURE_PORT;
+			}
+			else {
+				try {
+					securePort = Integer.valueOf(value);
+				} catch (NumberFormatException x) {
+					logger.warn("System property '{}' is set to the value '{}' which is not an integer. Falling back to default port {}.",
+							SYSTEM_PROPERTY_SECURE_PORT, value, DEFAULT_SECURE_PORT);
+					securePort = DEFAULT_SECURE_PORT;
+				}
+				if (securePort < 1 || securePort > 65535) {
+					logger.warn("System property '{}' is set to the value '{}' which is out of range. Falling back to default port {}.",
+							SYSTEM_PROPERTY_SECURE_PORT, securePort, DEFAULT_SECURE_PORT);
+					securePort = DEFAULT_SECURE_PORT;
+				}
+			}
+		}
+		return securePort;
+	}
+
+	public synchronized void setSecurePort(int securePort) {
+		assertNotRunning();
+		this.securePort = securePort;
+	}
+
+	private void assertNotRunning() {
+		if (running.get())
+			throw new IllegalStateException("Server is already running.");
+	}
+
 	private void initKeyStore() throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException, InvalidKeyException, SecurityException, SignatureException, NoSuchProviderException {
 		if (getKeyStoreFile().exists()) {
 			return;
@@ -104,26 +160,26 @@ public class CloudStoreServer implements Runnable {
 		ks.load(null, KEY_STORE_PASSWORD_CHAR_ARRAY); // TODO check if we can omit this password. makes no sense for security, anyway.
 
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-	    keyGen.initialize(4096, random); // TODO make configurable
-	    KeyPair pair = keyGen.generateKeyPair();
+		keyGen.initialize(4096, random); // TODO make configurable
+		KeyPair pair = keyGen.generateKeyPair();
 
-	    X509V3CertificateGenerator v3CertGen = new X509V3CertificateGenerator();
+		X509V3CertificateGenerator v3CertGen = new X509V3CertificateGenerator();
 
-	    long serial = new SecureRandom().nextLong();
+		long serial = new SecureRandom().nextLong();
 
-	    v3CertGen.setSerialNumber(BigInteger.valueOf(serial).abs());
-	    v3CertGen.setIssuerDN(new X509Principal("CN=" + CERTIFICATE_COMMON_NAME + ", OU=None, O=None, C=None"));
-	    v3CertGen.setNotBefore(new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24 * 30));
-	    v3CertGen.setNotAfter(new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365 * 10)));
-	    v3CertGen.setSubjectDN(new X509Principal("CN=" + CERTIFICATE_COMMON_NAME + ", OU=None, O=None, C=None"));
+		v3CertGen.setSerialNumber(BigInteger.valueOf(serial).abs());
+		v3CertGen.setIssuerDN(new X509Principal("CN=" + CERTIFICATE_COMMON_NAME + ", OU=None, O=None, C=None"));
+		v3CertGen.setNotBefore(new Date(System.currentTimeMillis() - (1000L * 60 * 60 * 24 * 3)));
+		v3CertGen.setNotAfter(new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365 * 10)));
+		v3CertGen.setSubjectDN(new X509Principal("CN=" + CERTIFICATE_COMMON_NAME + ", OU=None, O=None, C=None"));
 
-	    v3CertGen.setPublicKey(pair.getPublic());
-	    v3CertGen.setSignatureAlgorithm("SHA1WithRSAEncryption");
+		v3CertGen.setPublicKey(pair.getPublic());
+		v3CertGen.setSignatureAlgorithm("SHA1WithRSAEncryption");
 
-	    X509Certificate pkCertificate = v3CertGen.generateX509Certificate(pair.getPrivate());
+		X509Certificate pkCertificate = v3CertGen.generateX509Certificate(pair.getPrivate());
 
-	    PrivateKeyEntry entry = new PrivateKeyEntry(pair.getPrivate(), new Certificate[]{ pkCertificate });
-	    ks.setEntry(CERTIFICATE_ALIAS, entry, new KeyStore.PasswordProtection(KEY_PASSWORD_CHAR_ARRAY));
+		PrivateKeyEntry entry = new PrivateKeyEntry(pair.getPrivate(), new Certificate[]{ pkCertificate });
+		ks.setEntry(CERTIFICATE_ALIAS, entry, new KeyStore.PasswordProtection(KEY_PASSWORD_CHAR_ARRAY));
 
 		FileOutputStream fos = new FileOutputStream(getKeyStoreFile());
 		ks.store(fos, KEY_STORE_PASSWORD_CHAR_ARRAY);
@@ -132,52 +188,67 @@ public class CloudStoreServer implements Runnable {
 
 	private Server createServer() {
 		QueuedThreadPool threadPool = new QueuedThreadPool();
-        threadPool.setMaxThreads(500);
+		threadPool.setMaxThreads(500);
 
 		Server server = new Server(threadPool);
 		server.addBean(new ScheduledExecutorScheduler());
 
-        HttpConfiguration http_config = new HttpConfiguration();
-        http_config.setSecureScheme("https");
-        http_config.setSecurePort(8443);
-        http_config.setOutputBufferSize(32768);
-        http_config.setRequestHeaderSize(8192);
-        http_config.setResponseHeaderSize(8192);
-        http_config.setSendServerVersion(true);
-        http_config.setSendDateHeader(false);
+		HttpConfiguration http_config = createHttpConfigurationForHTTP();
 
 //        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
 //        http.setPort(8080);
 //        http.setIdleTimeout(30000);
 //        server.addConnector(http);
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		server.setHandler(context);
-		com.sun.jersey.spi.container.servlet.ServletContainer servletContainer = new com.sun.jersey.spi.container.servlet.ServletContainer(CloudStoreREST.class);
-		context.addServlet(new ServletHolder(servletContainer), "/*");
+		server.setHandler(createServletContextHandler());
+		server.setDumpAfterStart(false);
+		server.setDumpBeforeStop(false);
+		server.setStopAtShutdown(true);
 
-        server.setHandler(context);
-        server.setDumpAfterStart(false);
-        server.setDumpBeforeStop(false);
-        server.setStopAtShutdown(true);
-
-        SslContextFactory sslContextFactory = new SslContextFactory();
-        sslContextFactory.setKeyStorePath(getKeyStoreFile().getPath());
-        sslContextFactory.setKeyStorePassword(KEY_STORE_PASSWORD_STRING);
-        sslContextFactory.setKeyManagerPassword(KEY_PASSWORD_STRING);
-        sslContextFactory.setTrustStorePath(getKeyStoreFile().getPath());
-        sslContextFactory.setTrustStorePassword(KEY_STORE_PASSWORD_STRING);
-        sslContextFactory.setExcludeCipherSuites("SSL_RSA_WITH_DES_CBC_SHA", "SSL_DHE_RSA_WITH_DES_CBC_SHA", "SSL_DHE_DSS_WITH_DES_CBC_SHA", "SSL_RSA_EXPORT_WITH_RC4_40_MD5", "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA", ".*RC4.*");
-//        sslContextFactory.setCertAlias(CERTIFICATE_ALIAS); // Jetty uses our certificate. We put only one single cert into the key store. Hence, we don't need this.
-
-        HttpConfiguration https_config = new HttpConfiguration(http_config);
-        https_config.addCustomizer(new SecureRequestCustomizer());
-        ServerConnector sslConnector = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https_config));
-        sslConnector.setPort(8443);
-        server.addConnector(sslConnector);
+		HttpConfiguration https_config = createHttpConfigurationForHTTPS(http_config);
+		server.addConnector(createServerConnectorForHTTPS(server, https_config));
 
 		return server;
 	}
 
+	private HttpConfiguration createHttpConfigurationForHTTP() {
+		HttpConfiguration http_config = new HttpConfiguration();
+		http_config.setSecureScheme("https");
+		http_config.setSecurePort(getSecurePort());
+		http_config.setOutputBufferSize(32768);
+		http_config.setRequestHeaderSize(8192);
+		http_config.setResponseHeaderSize(8192);
+		http_config.setSendServerVersion(true);
+		http_config.setSendDateHeader(false);
+		return http_config;
+	}
+
+	private HttpConfiguration createHttpConfigurationForHTTPS(HttpConfiguration httpConfigurationForHTTP) {
+		HttpConfiguration https_config = new HttpConfiguration(httpConfigurationForHTTP);
+		https_config.addCustomizer(new SecureRequestCustomizer());
+		return https_config;
+	}
+
+	private ServletContextHandler createServletContextHandler() {
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		com.sun.jersey.spi.container.servlet.ServletContainer servletContainer = new com.sun.jersey.spi.container.servlet.ServletContainer(CloudStoreREST.class);
+		context.addServlet(new ServletHolder(servletContainer), "/*");
+		return context;
+	}
+
+	private ServerConnector createServerConnectorForHTTPS(Server server, HttpConfiguration httpConfigurationForHTTPS) {
+		SslContextFactory sslContextFactory = new SslContextFactory();
+		sslContextFactory.setKeyStorePath(getKeyStoreFile().getPath());
+		sslContextFactory.setKeyStorePassword(KEY_STORE_PASSWORD_STRING);
+		sslContextFactory.setKeyManagerPassword(KEY_PASSWORD_STRING);
+		sslContextFactory.setTrustStorePath(getKeyStoreFile().getPath());
+		sslContextFactory.setTrustStorePassword(KEY_STORE_PASSWORD_STRING);
+		sslContextFactory.setExcludeCipherSuites("SSL_RSA_WITH_DES_CBC_SHA", "SSL_DHE_RSA_WITH_DES_CBC_SHA", "SSL_DHE_DSS_WITH_DES_CBC_SHA", "SSL_RSA_EXPORT_WITH_RC4_40_MD5", "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA", ".*RC4.*");
+		//        sslContextFactory.setCertAlias(CERTIFICATE_ALIAS); // Jetty uses our certificate. We put only one single cert into the key store. Hence, we don't need this.
+
+		ServerConnector sslConnector = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(httpConfigurationForHTTPS));
+		sslConnector.setPort(getSecurePort());
+		return sslConnector;
+	}
 }
