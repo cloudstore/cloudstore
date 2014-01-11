a1063ced9a96d54e0ef8d017a3e48bb3df50d076
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-01-11 03:20:18 +0100

    Changed PasswordUtil to take only one exact length (not min + max).

--- co.codewizards.cloudstore.rest.server/src/main/java/co/codewizards/cloudstore/rest/server/auth/PasswordUtil.java
+++ co.codewizards.cloudstore.rest.server/src/main/java/co/codewizards/cloudstore/rest/server/auth/PasswordUtil.java
@@ -7,20 +7,11 @@ public final class PasswordUtil {
 
 	private static SecureRandom random = new SecureRandom();
 
-	private static final char[] PASSWORD_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/=.!?,;#$".toCharArray();
+	private static final char[] PASSWORD_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/=.!?,;#$()[]{}~".toCharArray();
 
-	public static char[] createRandomPassword(int minLength, int maxLength) {
-		if (minLength < 8)
-			throw new IllegalArgumentException("minLength < 8");
-
-		if (maxLength < minLength)
-			throw new IllegalArgumentException("maxLength < minLength");
-
-		int length;
-		if (minLength == maxLength)
-			length = maxLength;
-		else
-			length = minLength + random.nextInt(maxLength - minLength + 1);
+	public static char[] createRandomPassword(int length) {
+		if (length < 8)
+			throw new IllegalArgumentException("length < 8");
 
 		char[] result = new char[length];
 
--- co.codewizards.cloudstore.rest.server/src/test/java/co/codewizards/cloudstore/rest/server/auth/PasswordUtilTest.java
+++ co.codewizards.cloudstore.rest.server/src/test/java/co/codewizards/cloudstore/rest/server/auth/PasswordUtilTest.java
@@ -8,17 +8,16 @@ public class PasswordUtilTest {
 
 	@Test
 	public void assertValidMinAndMaxLength() {
-		int minLengthExpected = 20;
-		int maxLengthExpected = 30;
+		int lengthExpected = 30;
 		int minLengthActual = Integer.MAX_VALUE;
 		int maxLengthActual = 0;
 		for (int i = 0; i < 10000; ++i) {
-			char[] password = PasswordUtil.createRandomPassword(minLengthExpected, maxLengthExpected);
+			char[] password = PasswordUtil.createRandomPassword(lengthExpected);
 			minLengthActual = Math.min(minLengthActual, password.length);
 			maxLengthActual = Math.max(maxLengthActual, password.length);
 		}
-		assertThat(minLengthActual).isEqualTo(minLengthExpected);
-		assertThat(maxLengthActual).isEqualTo(maxLengthExpected);
+		assertThat(minLengthActual).isEqualTo(lengthExpected);
+		assertThat(maxLengthActual).isEqualTo(lengthExpected);
 	}
 
 }
