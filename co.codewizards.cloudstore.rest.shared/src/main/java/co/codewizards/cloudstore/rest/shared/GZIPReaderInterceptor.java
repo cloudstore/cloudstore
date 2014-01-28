7b1a7dda1e4220d96f76a1c19527d4699dd0170c
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-01-28 05:53:08 +0100

    Fixed GZIPReaderInterceptor for empty input streams.

--- co.codewizards.cloudstore.rest.shared/src/main/java/co/codewizards/cloudstore/rest/shared/GZIPReaderInterceptor.java
+++ co.codewizards.cloudstore.rest.shared/src/main/java/co/codewizards/cloudstore/rest/shared/GZIPReaderInterceptor.java
@@ -1,5 +1,6 @@
 package co.codewizards.cloudstore.rest.shared;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
@@ -8,13 +9,29 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.ext.ReaderInterceptor;
 import javax.ws.rs.ext.ReaderInterceptorContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GZIPReaderInterceptor implements ReaderInterceptor {
+	private static final Logger logger = LoggerFactory.getLogger(GZIPReaderInterceptor.class);
 
 	@Override
     public Object aroundReadFrom(ReaderInterceptorContext context)
                     throws IOException, WebApplicationException {
-        final InputStream originalInputStream = context.getInputStream();
-        context.setInputStream(new GZIPInputStream(originalInputStream));
+        InputStream originalInputStream = context.getInputStream();
+
+        if (!originalInputStream.markSupported())
+        	originalInputStream = new BufferedInputStream(originalInputStream);
+
+        originalInputStream.mark(5);
+        int read = originalInputStream.read();
+        originalInputStream.reset();
+
+        if (read > -1)
+        	context.setInputStream(new GZIPInputStream(originalInputStream));
+        else
+        	logger.debug("aroundReadFrom: originalInputStream is empty! Skipping GZIP.");
+
         return context.proceed();
     }
 
