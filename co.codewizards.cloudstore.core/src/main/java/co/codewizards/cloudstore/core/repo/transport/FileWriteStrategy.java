fa1c2244787b5e7304ad0eb04a681fbce45f5c32
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-03-27 21:54:08 +0100

    https://github.com/cloudstore/main/issues/19

--- co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/repo/transport/FileWriteStrategy.java
+++ co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/repo/transport/FileWriteStrategy.java
@@ -5,7 +5,7 @@ import co.codewizards.cloudstore.core.config.Config;
 /**
  * Strategy controlling how and when the destination file is written.
  * <p>
- * This is merely a setting in the {@link Config}. The the actual implementation is in the
+ * This is merely a setting in the {@link Config}. The actual implementation is in the
  * {@link co.codewizards.cloudstore.core.repo.transport.file.FileRepoTransport FileRepoTransport}.
  *
  * @author Marco หงุ่ยตระกูล-Schulze - marco at codewizards dot co
@@ -16,7 +16,8 @@ public enum FileWriteStrategy {
 	 * During transfer, the destination file is not touched.
 	 * <p>
 	 * This strategy requires as much temporary space in the destination file system as
-	 * blocks are transferred (in the worst case the entire file size).
+	 * blocks are transferred: In the worst case the total space requirement is thus twice
+	 * the file size (old file + all blocks).
 	 */
 	directAfterTransfer,
 
@@ -26,11 +27,19 @@ public enum FileWriteStrategy {
 	 * <p>
 	 * In contrast to {@link #directAfterTransfer} this may leave the destination file in an
 	 * inconsistent state for hours or even days - as long as the transfer takes.
+	 * <p>
+	 * However, this strategy requires the least space in the file system: Only once the file size.
+	 * There are no temporary files involved and thus no temporary space required.
 	 */
 	directDuringTransfer,
 
 	/**
-	 *
+	 * Same as {@link #directAfterTransfer}, but write a completely new file and then switch
+	 * the files (delete the old file and rename the new file).
+	 * <p>
+	 * This strategy is the safest concerning consistency, but requires the most temporary space in
+	 * the destination file system: In the worst case the total space requirement is thus 3 times
+	 * the file size (old file + all blocks + new file).
 	 */
 	replaceAfterTransfer
 	;
