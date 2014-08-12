9764ac05a75cbe3f281a26fb41988e71fdadfbff
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-08-12 18:25:16 +0200

    More javadoc.

---
+++ co.codewizards.cloudstore.rest.client/src/main/java/co/codewizards/cloudstore/rest/client/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * REST client for REST-based client-server-communication.
+ * <p>
+ * The most important class in this package is
+ * {@link co.codewizards.cloudstore.rest.client.CloudStoreRestClient CloudStoreRestClient} facilitating the
+ * {@linkplain co.codewizards.cloudstore.rest.client.CloudStoreRestClient CloudStoreRestClient#execute(co.codewizards.cloudstore.rest.client.request.Request) execution}
+ * of REST requests.
+ */
+package co.codewizards.cloudstore.rest.client;
