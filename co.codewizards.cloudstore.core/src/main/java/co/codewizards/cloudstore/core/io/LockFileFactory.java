9738d5ab06a62aca33313f55f5f56264d9d83d57
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-08-13 11:44:44 +0200

    Removed unnecessary import used solely in Javadoc.

--- co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/io/LockFileFactory.java
+++ co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/io/LockFileFactory.java
@@ -1,10 +1,9 @@
 package co.codewizards.cloudstore.core.io;
 
-import static co.codewizards.cloudstore.core.util.Util.*;
+import static co.codewizards.cloudstore.core.util.Util.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.channels.FileLock;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,7 +27,7 @@ public class LockFileFactory {
 		return LockFileFactoryHolder.instance;
 	}
 
-	private Map<File, LockFileImpl> file2LockFileImpl = new HashMap<File, LockFileImpl>();
+	private final Map<File, LockFileImpl> file2LockFileImpl = new HashMap<File, LockFileImpl>();
 
 	/**
 	 * Acquire an exclusive lock on the specified file.
@@ -47,8 +46,8 @@ public class LockFileFactory {
 	 * <p>
 	 * <b>Important:</b> This is <i>not</i> usable for the synchronization of multiple threads within the same Java virtual machine!
 	 * Multiple {@link LockFile}s on the same {@link File} are possible within the same JVM! This locking mechanism
-	 * only locks against separate processes! Since this implementation is based on {@link FileLock}, please consult
-	 * its Javadoc for further information.
+	 * only locks against separate processes! Since this implementation is based on {@link java.nio.channels.FileLock FileLock},
+	 * please consult its Javadoc for further information.
 	 * <p>
 	 * Multiple invocations of this method on the same given {@code file} return multiple different {@code LockFile} instances.
 	 * The actual lock is held until the last {@code LockFile} instance was {@linkplain LockFile#release() released}.
@@ -58,16 +57,17 @@ public class LockFileFactory {
 	 * it is created by this method.
 	 * @param timeoutMillis the timeout to wait for the lock to be acquired in milliseconds. The value 0 means to
 	 * wait forever.
-	 * @return the {@code LockFile}. Never <code>null</code>. This <i>must</i> be {@linkplain FileLock#release() released}
+	 * @return the {@code LockFile}. Never <code>null</code>. This <i>must</i> be
+	 * {@linkplain java.nio.channels.FileLock#release() released}
 	 * (use a try-finally-block)!
 	 * @throws TimeoutException if the {@code LockFile} could not be acquired within the timeout specified by {@code timeoutMillis}.
 	 * @see LockFile#release()
 	 */
-	public LockFile acquire(File file, long timeoutMillis) throws TimeoutException {
+	public LockFile acquire(File file, final long timeoutMillis) throws TimeoutException {
 		assertNotNull("file", file);
 		try {
 			file = file.getCanonicalFile();
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			throw new RuntimeException(e);
 		}
 
@@ -86,8 +86,8 @@ public class LockFileFactory {
 			lockFileImpl.acquire(timeoutMillis);
 		} finally {
 			synchronized (this) {
-				int lockCounter = lockFileImpl.getLockCounter();
-				int acquireRunningCounter = lockFileImpl.acquireRunningCounter.decrementAndGet();
+				final int lockCounter = lockFileImpl.getLockCounter();
+				final int acquireRunningCounter = lockFileImpl.acquireRunningCounter.decrementAndGet();
 
 				if (lockCounter < 1 && acquireRunningCounter < 1)
 					file2LockFileImpl.remove(file);
@@ -96,13 +96,13 @@ public class LockFileFactory {
 		return new LockFileProxy(lockFileImpl);
 	}
 
-	protected synchronized void postRelease(LockFileImpl lockFileImpl) {
-		LockFileImpl lockFileImpl2 = file2LockFileImpl.get(lockFileImpl.getFile());
+	protected synchronized void postRelease(final LockFileImpl lockFileImpl) {
+		final LockFileImpl lockFileImpl2 = file2LockFileImpl.get(lockFileImpl.getFile());
 		if (lockFileImpl != lockFileImpl2)
 			throw new IllegalArgumentException("Unknown lockFileImpl instance (not managed by this registry): " + lockFileImpl);
 
-		int lockCounter = lockFileImpl.getLockCounter();
-		int acquireRunningCounter = lockFileImpl.acquireRunningCounter.decrementAndGet();
+		final int lockCounter = lockFileImpl.getLockCounter();
+		final int acquireRunningCounter = lockFileImpl.acquireRunningCounter.decrementAndGet();
 
 		if (lockCounter < 1 && acquireRunningCounter < 1)
 			file2LockFileImpl.remove(lockFileImpl.getFile());
