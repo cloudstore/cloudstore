be161253456f1e928a8e2a2f30e8ac74a123f710
Marco หงุ่ยตระกูล-Schulze <marco@nightlabs.de>
2015-04-27 22:18:29 +0700

    Test DTO.

--- co.codewizards.cloudstore.ls.core/src/main/java/co/codewizards/cloudstore/ls/core/dto/TestDto.java
+++ co.codewizards.cloudstore.ls.core/src/main/java/co/codewizards/cloudstore/ls/core/dto/TestDto.java
@@ -1,9 +1,17 @@
 package co.codewizards.cloudstore.ls.core.dto;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class TestDto {
 
-	public TestDto() {
-		// TODO Auto-generated constructor stub
+	private String bla;
+
+	public String getBla() {
+		return bla;
+	}
+	public void setBla(String bla) {
+		this.bla = bla;
 	}
 
 }
