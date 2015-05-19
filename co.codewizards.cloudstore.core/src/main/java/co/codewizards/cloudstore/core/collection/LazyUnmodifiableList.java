103cb3eb5e0c9516518cc070884aee05d69da52d
Marco หงุ่ยตระกูล-Schulze <marco@nightlabs.de>
2015-05-19 14:03:45 +0700

    Removed java 8 stuff to make it compilable with java 7.

--- co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/collection/LazyUnmodifiableList.java
+++ co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/collection/LazyUnmodifiableList.java
@@ -5,24 +5,18 @@ import static co.codewizards.cloudstore.core.util.AssertUtil.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-import java.util.stream.Stream;
 
 public abstract class LazyUnmodifiableList<E> implements List<E> {
 
 	private List<E> elements;
 
-	@Override
-	public void forEach(Consumer<? super E> action) {
-		getElements().forEach(action);
-	}
+//	@Override
+//	public void forEach(Consumer<? super E> action) {
+//		getElements().forEach(action);
+//	}
 
 	@Override
 	public int size() {
@@ -89,20 +83,20 @@ public abstract class LazyUnmodifiableList<E> implements List<E> {
 		return getElements().retainAll(c);
 	}
 
-	@Override
-	public void replaceAll(UnaryOperator<E> operator) {
-		getElements().replaceAll(operator);
-	}
+//	@Override
+//	public void replaceAll(UnaryOperator<E> operator) {
+//		getElements().replaceAll(operator);
+//	}
 
-	@Override
-	public boolean removeIf(Predicate<? super E> filter) {
-		return getElements().removeIf(filter);
-	}
+//	@Override
+//	public boolean removeIf(Predicate<? super E> filter) {
+//		return getElements().removeIf(filter);
+//	}
 
-	@Override
-	public void sort(Comparator<? super E> c) {
-		getElements().sort(c);
-	}
+//	@Override
+//	public void sort(Comparator<? super E> c) {
+//		getElements().sort(c);
+//	}
 
 	@Override
 	public void clear() {
@@ -134,20 +128,20 @@ public abstract class LazyUnmodifiableList<E> implements List<E> {
 		getElements().add(index, element);
 	}
 
-	@Override
-	public Stream<E> stream() {
-		return getElements().stream();
-	}
+//	@Override
+//	public Stream<E> stream() {
+//		return getElements().stream();
+//	}
 
 	@Override
 	public E remove(int index) {
 		return getElements().remove(index);
 	}
 
-	@Override
-	public Stream<E> parallelStream() {
-		return getElements().parallelStream();
-	}
+//	@Override
+//	public Stream<E> parallelStream() {
+//		return getElements().parallelStream();
+//	}
 
 	@Override
 	public int indexOf(Object o) {
@@ -174,10 +168,10 @@ public abstract class LazyUnmodifiableList<E> implements List<E> {
 		return getElements().subList(fromIndex, toIndex);
 	}
 
-	@Override
-	public Spliterator<E> spliterator() {
-		return getElements().spliterator();
-	}
+//	@Override
+//	public Spliterator<E> spliterator() {
+//		return getElements().spliterator();
+//	}
 
 	protected List<E> getElements() {
 		if (elements == null)
