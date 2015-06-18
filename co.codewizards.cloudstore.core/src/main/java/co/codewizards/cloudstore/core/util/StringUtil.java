a0512f7175cc1364c90b3c2677baa2de640df10e
Marco หงุ่ยตระกูล-Schulze <marco@nightlabs.de>
2015-06-18 12:01:30 +0700

    Added a few useful methods to StringUtil.

--- co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/util/StringUtil.java
+++ co.codewizards.cloudstore.core/src/main/java/co/codewizards/cloudstore/core/util/StringUtil.java
@@ -8,7 +8,7 @@ public final class StringUtil {
 
 	private StringUtil() { }
 
-	public static List<Integer> getIndexesOf(String string, char c) {
+	public static final List<Integer> getIndexesOf(final String string, final char c) {
 		ArrayList<Integer> indexes = new ArrayList<Integer>();
 		for (int index = 0; index < string.length(); ++index) {
 			if (string.charAt(index) == c)
@@ -18,7 +18,39 @@ public final class StringUtil {
 		return Collections.unmodifiableList(indexes);
 	}
 
-	public static boolean isEmpty(String string) {
+	public static final boolean isEmpty(final String string) {
 		return string == null || string.isEmpty();
 	}
+
+	public static final String nullToEmpty(final String string) {
+		return string == null ? "" : string;
+	}
+
+	public static final String emptyToNull(final String string) {
+		return isEmpty(string) ? null : string;
+	}
+
+	public static final String trim(final String string) {
+		return string == null ? null : string.trim();
+	}
+
+	public static final String trimLeft(final String string) {
+        final int len = string.length();
+        int st = 0;
+
+        while ((st < len) && (string.charAt(st) <= ' '))
+            st++;
+
+        return st > 0 ? string.substring(st) : string;
+    }
+
+	public static final String trimRight(final String string) {
+		final int length = string.length();
+		int len = length;
+
+        while ((0 < len) && (string.charAt(len - 1) <= ' '))
+            len--;
+
+        return len < length ? string.substring(0, len) : string;
+    }
 }
