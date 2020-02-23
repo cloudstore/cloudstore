78aeef26168f7ef4c4944800d087b0467a325e3e
Marco Nguitragool <marco@codewizards.co>
2020-02-23 10:35:28 +0100

    1.0.0 release

--- co.codewizards.cloudstore.release/src/main/java/co/codewizards/cloudstore/release/ReleasePreparer.java
+++ co.codewizards.cloudstore.release/src/main/java/co/codewizards/cloudstore/release/ReleasePreparer.java
@@ -29,8 +29,8 @@ public class ReleasePreparer {
 	//   * to update the symlinks on the web-server after the release was successful;
 	//   * to update the auto-update-meta-files on the web-server.
 
-//	protected String newMavenVersion = "1.0.0";
-	protected String newMavenVersion = "1.0.0-SNAPSHOT";
+	protected String newMavenVersion = "1.0.0";
+//	protected String newMavenVersion = "1.0.1-SNAPSHOT";
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// DO NOT CHANGE ANYTHING BELOW THIS POINT, if you don't really want to improve this program.
