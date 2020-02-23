15b409bed053624e84afc1b3b00af5f607b53642
Marco Nguitragool <marco@codewizards.co>
2020-02-23 05:10:08 +0100

    https://github.com/cloudstore/cloudstore/issues/88

--- co.codewizards.cloudstore.local/src/main/java/co/codewizards/cloudstore/local/dbupdate/DbUpdateStep003.java
+++ co.codewizards.cloudstore.local/src/main/java/co/codewizards/cloudstore/local/dbupdate/DbUpdateStep003.java
@@ -1,7 +1,11 @@
 package co.codewizards.cloudstore.local.dbupdate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
+import co.codewizards.cloudstore.core.oio.File;
+import co.codewizards.cloudstore.core.util.IOUtil;
 import co.codewizards.cloudstore.core.util.PropertiesUtil;
 
 /**
@@ -19,27 +23,50 @@ public class DbUpdateStep003 extends AbstractDbUpdateStep {
 
 	@Override
 	public void performUpdate() throws Exception {
-		Properties persistenceProperties = PropertiesUtil.load(getPersistencePropertiesFile());
-		
-		persistenceProperties.setProperty("datanucleus.schema.autoCreateDatabase", "true");
-		persistenceProperties.setProperty("datanucleus.schema.autoCreateTables", "true");
-		persistenceProperties.setProperty("datanucleus.schema.autoCreateColumns", "true");
-		persistenceProperties.setProperty("datanucleus.schema.autoCreateConstraints", "true");
-		
-		persistenceProperties.remove("datanucleus.autoCreateSchema");
-		persistenceProperties.remove("datanucleus.autoCreateTables");
-		persistenceProperties.remove("datanucleus.autoCreateColumns");
-		persistenceProperties.remove("datanucleus.autoCreateConstraints");
-		
-		
-		persistenceProperties.setProperty("datanucleus.schema.validateTables", "true");
-		persistenceProperties.setProperty("datanucleus.schema.validateColumns", "true");
-		persistenceProperties.setProperty("datanucleus.schema.validateConstraints", "true");
-		
-		persistenceProperties.remove("datanucleus.validateTables");
-		persistenceProperties.remove("datanucleus.validateColumns");
-		persistenceProperties.remove("datanucleus.validateConstraints");
-		
-		PropertiesUtil.store(getPersistencePropertiesFile(), persistenceProperties, null);
+		File persistencePropertiesFile = getPersistencePropertiesFile();
+
+		// Before we change anything, we copy the original persisitence-properties-file into a backup-file.
+		String dateString = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
+		File persistencePropertiesBackup = persistencePropertiesFile.getParentFile().createFile(
+				persistencePropertiesFile.getName() + "." + dateString + ".bak");
+
+		IOUtil.copyFile(persistencePropertiesFile, persistencePropertiesBackup);
+
+		Properties persistenceProperties = PropertiesUtil.load(persistencePropertiesFile);
+
+		String connectionUrl = persistenceProperties.getProperty("javax.jdo.option.ConnectionURL");
+		if (connectionUrl != null && connectionUrl.contains(":derby:")) {
+			// At the moment, we officially support only derby. Hence, it should always copy the newer
+			// template-file. This is much nicer than reading+writing a Properties object, which causes
+			// all comments to be lost and which chaotically reorders all the settings.
+			IOUtil.copyResource(this.getClass(), "/cloudstore-persistence.derby.properties",
+					getPersistencePropertiesFile());
+		}
+		else {
+			// In case, downstream-projects use sth. other than derby, we migrate the existing file,
+			// instead of copying the template (which we don't know here). This is not beautiful,
+			// but it should work (and I tested this code -- it really does work).
+			persistenceProperties.setProperty("datanucleus.schema.autoCreateDatabase", "true");
+			persistenceProperties.setProperty("datanucleus.schema.autoCreateTables", "true");
+			persistenceProperties.setProperty("datanucleus.schema.autoCreateColumns", "true");
+			persistenceProperties.setProperty("datanucleus.schema.autoCreateConstraints", "true");
+			
+			persistenceProperties.remove("datanucleus.autoCreateSchema");
+			persistenceProperties.remove("datanucleus.autoCreateTables");
+			persistenceProperties.remove("datanucleus.autoCreateColumns");
+			persistenceProperties.remove("datanucleus.autoCreateConstraints");
+			
+			
+			persistenceProperties.setProperty("datanucleus.schema.validateTables", "true");
+			persistenceProperties.setProperty("datanucleus.schema.validateColumns", "true");
+			persistenceProperties.setProperty("datanucleus.schema.validateConstraints", "true");
+			
+			persistenceProperties.remove("datanucleus.validateTables");
+			persistenceProperties.remove("datanucleus.validateColumns");
+			persistenceProperties.remove("datanucleus.validateConstraints");
+			
+			PropertiesUtil.store(persistencePropertiesFile, persistenceProperties, null);
+		}
 	}
 }
