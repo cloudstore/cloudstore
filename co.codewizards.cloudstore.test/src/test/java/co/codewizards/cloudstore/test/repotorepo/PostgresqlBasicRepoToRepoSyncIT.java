e56f5ef609487f1e45629cfb04792269753051c3
Marco Nguitragool <marco@codewizards.co>
2020-04-13 10:51:08 +0200

    https://github.com/cloudstore/cloudstore/issues/90

---
+++ co.codewizards.cloudstore.test/src/test/java/co/codewizards/cloudstore/test/repotorepo/PostgresqlBasicRepoToRepoSyncIT.java
@@ -0,0 +1,69 @@
+package co.codewizards.cloudstore.test.repotorepo;
+
+import static co.codewizards.cloudstore.local.db.DatabaseAdapterFactory.*;
+import static co.codewizards.cloudstore.local.db.ExternalJdbcDatabaseAdapter.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import co.codewizards.cloudstore.core.config.Config;
+
+public class PostgresqlBasicRepoToRepoSyncIT extends BasicRepoToRepoSyncIT {
+
+	@BeforeClass
+	public static void before_PostgresqlDatabaseAdapterTest() {
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_DATABASE_ADAPTER_NAME, "postgresql");
+
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_HOST_NAME, getEnvOrFail("TEST_PG_HOST_NAME"));
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_USER_NAME, getEnvOrFail("TEST_PG_USER_NAME"));
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_PASSWORD, getEnvOrFail("TEST_PG_PASSWORD"));
+
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_DB_NAME_PREFIX, "TEST_");
+		System.setProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_DB_NAME_SUFFIX, "_TEST");
+	}
+
+	@AfterClass
+	public static void after_PostgresqlDatabaseAdapterTest() {
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_DATABASE_ADAPTER_NAME);
+
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_HOST_NAME);
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_USER_NAME);
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_PASSWORD);
+
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_DB_NAME_PREFIX);
+		System.clearProperty(Config.SYSTEM_PROPERTY_PREFIX + CONFIG_KEY_JDBC_DB_NAME_SUFFIX);
+	}
+
+	protected static String getEnvOrFail(String key) {
+		String value = System.getenv(key);
+		if (value == null)
+			throw new IllegalStateException("Environment-variable not set: " + key);
+
+		return value;
+	}
+
+	@Override
+	@Test
+	public void syncFromRemoteToLocal() throws Exception {
+		super.syncFromRemoteToLocal();
+	}
+
+	@Override
+	@Test
+	public void syncFromLocalToRemote() throws Exception {
+		super.syncFromLocalToRemote();
+	}
+
+	@Override
+	@Test
+	public void syncMovedFile() throws Exception {
+		super.syncMovedFile();
+	}
+
+	@Override
+	@Test
+	public void syncMovedFileToNewDir() throws Exception {
+		super.syncMovedFileToNewDir();
+	}
+}
